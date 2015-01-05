87cf98a8b865a916276a4be18f5909ba631a8d5f
Prashant Shah <pshah.webcbt@gmail.com>
2015-01-05 20:43:10 +0530

    Add deleted_at column to tags table
    Signed-off-by: Prashant Shah <pshah.webcbt@gmail.com>

--- app/database/schema.sql
+++ app/database/schema.sql
@@ -206,6 +206,7 @@ CREATE TABLE IF NOT EXISTS `tags` (
   `background` char(6) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
