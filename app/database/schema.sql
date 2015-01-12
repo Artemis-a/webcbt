bc73eb7bad545921c749a39114c0562027cbd311
Prashant Shah <pshah.webcbt@gmail.com>
2015-01-12 15:19:22 +0530

    Update schema
    Signed-off-by: Prashant Shah <pshah.webcbt@gmail.com>

--- app/database/schema.sql
+++ app/database/schema.sql
@@ -23,108 +23,33 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `cbts`
+-- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `cbts` (
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  `situation` varchar(255) NOT NULL,
-  `is_resolved` int(1) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `deleted_at` timestamp NULL DEFAULT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `gender` char(1) NOT NULL,
+  `dob` date NOT NULL,
+  `dateformat` varchar(255) NOT NULL,
+  `timezone` varchar(255) NOT NULL,
+  `last_login` datetime NOT NULL,
+  `options` varchar(255) NOT NULL,
+  `status` int(1) NOT NULL,
+  `verification_key` varchar(255) NOT NULL,
+  `email_verified` int(1) NOT NULL,
+  `admin_verified` int(1) NOT NULL,
+  `retry_count` int(1) NOT NULL,
+  `reset_password_key` varchar(255) DEFAULT NULL,
+  `reset_password_date` datetime DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `deleted_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cbt_behaviours`
---
-
-CREATE TABLE IF NOT EXISTS `cbt_behaviours` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cbt_id` int(11) NOT NULL,
-  `behaviour` varchar(255) NOT NULL,
-  `status` char(1) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cbt_feelings`
---
-
-CREATE TABLE IF NOT EXISTS `cbt_feelings` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cbt_id` int(11) NOT NULL,
-  `feeling_id` int(11) NOT NULL,
-  `intensity` int(3) NOT NULL,
-  `status` char(1) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cbt_symptoms`
---
-
-CREATE TABLE IF NOT EXISTS `cbt_symptoms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cbt_id` int(11) NOT NULL,
-  `symptom_id` int(11) NOT NULL,
-  `intensity` int(3) NOT NULL,
-  `status` char(1) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cbt_thoughts`
---
-
-CREATE TABLE IF NOT EXISTS `cbt_thoughts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cbt_id` int(11) NOT NULL,
-  `thought` varchar(255) NOT NULL,
-  `is_disputed` int(1) NOT NULL,
-  `dispute` varchar(2000) NOT NULL,
-  `balanced_thoughts` varchar(255) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cbt_thought_distortions`
---
-
-CREATE TABLE IF NOT EXISTS `cbt_thought_distortions` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `cbt_thought_id` int(11) NOT NULL,
-  `distortion_id` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `deleted_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
 
 -- --------------------------------------------------------
 
@@ -215,33 +140,109 @@ CREATE TABLE IF NOT EXISTS `tags` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `cbts`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `cbts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `fullname` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `gender` char(1) NOT NULL,
-  `dob` date NOT NULL,
-  `dateformat` varchar(255) NOT NULL,
-  `timezone` varchar(255) NOT NULL,
-  `last_login` datetime NOT NULL,
-  `options` varchar(255) NOT NULL,
-  `status` int(1) NOT NULL,
-  `verification_key` varchar(255) NOT NULL,
-  `email_verified` int(1) NOT NULL,
-  `admin_verified` int(1) NOT NULL,
-  `retry_count` int(1) NOT NULL,
-  `reset_password_key` varchar(255) DEFAULT NULL,
-  `reset_password_date` datetime DEFAULT NULL,
-  `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL,
-  `deleted_at` datetime DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `tag_id` int(11) DEFAULT NULL,
+  `date` datetime NOT NULL,
+  `situation` varchar(255) NOT NULL,
+  `is_resolved` int(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cbt_thoughts`
+--
+
+CREATE TABLE IF NOT EXISTS `cbt_thoughts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cbt_id` int(11) NOT NULL,
+  `thought` varchar(255) NOT NULL,
+  `is_disputed` int(1) NOT NULL,
+  `dispute` varchar(2000) NOT NULL,
+  `balanced_thoughts` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cbt_feelings`
+--
+
+CREATE TABLE IF NOT EXISTS `cbt_feelings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cbt_id` int(11) NOT NULL,
+  `feeling_id` int(11) NOT NULL,
+  `intensity` int(3) NOT NULL,
+  `status` char(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cbt_symptoms`
+--
+
+CREATE TABLE IF NOT EXISTS `cbt_symptoms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cbt_id` int(11) NOT NULL,
+  `symptom_id` int(11) NOT NULL,
+  `intensity` int(3) NOT NULL,
+  `status` char(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cbt_behaviours`
+--
+
+CREATE TABLE IF NOT EXISTS `cbt_behaviours` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cbt_id` int(11) NOT NULL,
+  `behaviour` varchar(255) NOT NULL,
+  `status` char(1) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cbt_thought_distortions`
+--
+
+CREATE TABLE IF NOT EXISTS `cbt_thought_distortions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cbt_thought_id` int(11) NOT NULL,
+  `distortion_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
