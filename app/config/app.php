1043445809fe534416f64efe1570f10d0728def0
Prashant Shah <pshah.webcbt@gmail.com>
2015-01-13 15:24:58 +0530

    Disable debugbar
    Signed-off-by: Prashant Shah <pshah.webcbt@gmail.com>

--- app/config/app.php
+++ app/config/app.php
@@ -122,7 +122,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Manavo\BootstrapForms\BootstrapFormsServiceProvider',
-		'Barryvdh\Debugbar\ServiceProvider',
+		// 'Barryvdh\Debugbar\ServiceProvider',
 
 	),
 
